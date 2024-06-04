3a7e109405f2ebe325caf9d0a0ac3aa143cb19d9
Chaveejay <chawwa53@gmail.com>
2024-06-05 02:16:02 +0530

    Order now Card Creation

--- services.php
+++ services.php
@@ -5,9 +5,14 @@
     <link rel="icon" href="images\icon.png" type="image/gif" />
     <title>Signature Cuisine</title>
     <link rel="stylesheet" href="css\style.css" >
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
 
     <script src="http://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
 
@@ -52,22 +57,38 @@
                 <div class="card-img">
                 <img src="images/food/spaghetti.jpg" alt="" class="img-img">
                 </div>
-                <h4 class="card-title">Title 1</h3>
-                <div class="card-text">text</div>
+                <h6 class="card-title">Spaghetti </h6>
+                <div class="card-text">Spaghetti bolognese is a pasta dish of spaghetti with 
+                    an Italian meat sauce called ragù, served with Parmesan cheese. 
+                    The sauce is made with minced beef or other ground meat, tomatoes, 
+                    onions, carrots, celery, milk, garlic, wine, and herbs, and is usually
+                     slow-cooked for several hours to develop the flavors.</div>
+                     <div class="price">Rs.1500</div>
+            </div>
+            
+            <div class="card">
+                <div class="card-img">
+                <img src="images/food/fried rice.jpg" alt="" class="img-img">
+                </div>
+                <h6 class="card-title">Chicken Fried Rice</h3>
+                <div class="card-text">The perfect fried chicken is crispy on the outside,
+                     with a flavourful crust that contrasts and complements the tender 
+                     chicken it contains. The starch is deeply browned, allowing the complex 
+                     flavours created by the Maillard reaction to shine through, without 
+                     letting anything burn.</div>
+                     <div class="price">Rs.1200</div>
             </div>
             <div class="card">
                 <div class="card-img">
-                <img src="images/6798.jpg" alt="" class="img-img">
+                    <img src="images/food/noodles.jpg" alt="" class="img-img">
                 </div>
-                <h4 class="card-title">Title 2</h3>
-                <div class="card-text">text</div>
+                <h6 class="card-title">Chicken Noodles</h3>
+                <div class="card-text">Chicken noodles is a flavorful and satisfying dish 
+                    featuring tender pieces of chicken stir-fried with a mix of fresh vegetables
+                     and egg noodles. The dish is typically seasoned with a savory blend of soy sauce,
+                      garlic, ginger, and a touch of sesame oil, creating a perfect balance of flavors.</div>
+                <div class="price">Rs.1200</div>
             </div>
-            <div class="card">
-                <div class="card-img">
-                    <img src="images/6471.jpg" alt="" class="img-img">
-                </div>
-                <h4 class="card-title">Title 3.1</h3>
-                <div class="card-text">text</div>
-            </div>
+            
         </div>
     </section>
