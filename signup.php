1807acac9c1b0f8b963a5a77b41fee8592f839b1
Chaveejay <chawwa53@gmail.com>
2024-06-06 00:04:50 +0530

    Sign up creation

--- signup.php
+++ signup.php
@@ -8,24 +8,16 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css\login.css" >
-    <title>Login</title>
+    <link rel="stylesheet" href="css\signup.css" >
+    <title>SignUP</title>
 </head>
 <body>
     <div class="main-box">
         <div class="login">
-            <div class="sign-in">
-                Sign In
+            <div class="sign-up">
+                Sign UP
             </div>
-            <div class="dropdown">
-                <div class="usertype">UserType</div>
-
-            <select name="language" id="drop-down">
-            <option value="Admin">Admin</option>
-            <option value="Staff">Staff</option>
-            <option value="User" >User</option>
-            </select>
-            </div>
+            
             <div class="form">
             <form action="">
                 <div class="username">
@@ -33,17 +25,25 @@
             </div>
             <input type="text" id="username" name="username" ><br>
             <div class="password">
+            <label for="email">Email</label><br>
+            </div>
+            <input type="text" id="email" name="email" ><br>
+            <div class="password">
             <label for="password">Password</label><br>
             </div>
-            <input type="password" id="password" name="password" ><br><br>
-            <input type="submit" class="signin-btn" value="Sign IN">
+            <input type="password" id="password" name="password" ><br>
+            <div class="password">
+            <label for="confirm-password">Confirm Password</label><br>
+            </div>
+            <input type="password" id="confirm-password" name="confirm-password" ><br><br>
+            <input type="submit" class="signup-btn" value="Sign UP">
             </form> 
             </div>
         </div>
-        <div class="sign-up">
-            <div class="welcome"> Welcome to Login </div>
-            <div class="paragraph"> Don't have an account?</div>
-            <a href="signup.php" class=""><input type="submit" class="signup-btn" value="Sign UP"></a>
+        <div class="sign-in">
+            <div class="welcome"> Welcome to SignUP </div>
+            <div class="paragraph"> Already have an account?</div>
+            <a href="login.php"><input type="submit" class="signin-btn" value="Sign IN"></a>
 
         </div>
 
