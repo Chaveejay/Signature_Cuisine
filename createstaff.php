29e6e0651c451ecf1d29d65198f60cb05326f2a0
Chaveejay <chawwa53@gmail.com>
2024-06-12 23:38:21 +0530

    Create staff account

---
+++ createstaff.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Staff Account</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css\admin.css" >
+</head>
+<body>
+     <!------------------Header------------------------->
+    <div class="headbar">
+            <!----logo------------->
+            <a href="admin.php" class="logo">
+                Signature<span> Cuisine</span>
+            </a>
+    </div>
+    <!------------------Admin Panel------------------------->
+    <section id="create-staff">
+        <div class="staff-form">
+        <form action="">
+            
+            <input type="text" id="fname" name="fname" placeholder =" First Name" >
+            <input type="text" id="lname" name="lname" placeholder =" Last Name" >
+            
+        <!-- <label for="fname">First name:</label>
+        <input type="text" id="fname" name="fname" ><br>
+
+        <label for="lname">Last name:</label>
+        <input type="text" id="lname" name="lname"><br>
+        <input type="submit" value="Submit"> -->
+        </form> 
+        </div>
+
+
+    </section>
